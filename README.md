4ae38d01f12967812eb47cca7e4ea68b14882afb
Daniel_Ritter <dritter@mail.com>
2024-08-10 16:58:09 +0200

    development draft & first model train script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # conti-mle
 Homework for Senior Machine Learning Engineer - DevOps focus
+
+This repository demonstrates the ML workflow and deployment of an Fashion MNIST image recognition model and service.
+
+* The model is trained with PyTorch Lightning and Torchvision libraries.
+* The workflow is orchestrated with GitHub Actions.
+  (GHA is preferred over Airflow for the orchestration because of the model's lightweight requirements)
+
+The model training uses CrossEntropyLoss as a loss function, Adam (Adaptive Moment Estimation) as an optimiser, and Early stop to avoid overfitting the model.
---
+++ training/FashionMNIST-train.py
@@ -0,0 +1,76 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import EarlyStopping
+import time
+
+start = time.time()
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+train_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64)
+
+
+class LitFashionMNIST(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(28 * 28, 128)
+        self.fc2 = torch.nn.Linear(128, 64)
+        self.fc3 = torch.nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = torch.nn.CrossEntropyLoss()(y_hat, y)
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = torch.nn.CrossEntropyLoss()(y_hat, y)
+        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = torch.nn.CrossEntropyLoss()(y_hat, y)
+        self.log('test_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
+
+
+model = LitFashionMNIST()
+
+early_stop_callback = EarlyStopping(
+    monitor='val_loss',
+    min_delta=0.00,
+    patience=3,
+    verbose=True,
+    mode='min'
+)
+
+trainer = Trainer(max_epochs=100, accelerator="auto", callbacks=[early_stop_callback])
+
+trainer.fit(model, train_loader, test_loader)
+trainer.test(model, dataloaders=test_loader)
+
+
+end = time.time()
+print("Execution time in seconds: " + str(end - start))
