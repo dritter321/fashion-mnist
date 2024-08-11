7551a591da45a4a3b7031a53a30f2715268c59c2
Daniel_Ritter <dritter@mail.com>
2024-08-11 18:55:54 +0200

    fixing unittest

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -7,10 +7,12 @@ from utils.logger import log_run_details_to_file
 class TestLogger(unittest.TestCase):
 
     def test_log_run_details_to_file_creates_file_if_not_exists(self):
+        os.makedirs("mlruns", exist_ok=True)
         log_run_details_to_file("test_experiment", "123", "456", 0.9, "/path/to/model")
         self.assertTrue(os.path.exists("./mlruns/run_logs.csv"))
 
     def test_log_run_details_to_file_appends_to_existing_file(self):
+        os.makedirs("mlruns", exist_ok=True)
         log_run_details_to_file("test_experiment", "123", "456", 0.9, "/path/to/model")
         header_expected = "timestamp,experiment_name,experiment_id,run_id,execution_time,model_path"
         with open("./mlruns/run_logs.csv", 'r') as file:
