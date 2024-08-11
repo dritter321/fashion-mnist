27724867fef55a08d9e4a03694a91ae7307e5a2d
Daniel_Ritter <dritter@mail.com>
2024-08-11 18:50:46 +0200

    fixing unit test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import patch, mock_open
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -7,28 +6,17 @@ from utils.logger import log_run_details_to_file
 
 class TestLogger(unittest.TestCase):
 
-    @patch("utils.logger.os.path.exists")
-    @patch("utils.logger.open", new_callable=mock_open())
     def test_log_run_details_to_file_creates_file_if_not_exists(self, mock_file, mock_exists):
-        mock_exists.return_value = False
         log_run_details_to_file("test_experiment", "123", "456", 0.9, "/path/to/model")
+        self.assertTrue(os.path.exists("./mlruns/run_logs.csv"))
 
-        mock_file.assert_called_once_with(os.path.join("./mlruns", "run_logs.csv"), 'w')
-        handle = mock_file()
-        handle.write.assert_called_with("timestamp,experiment_name,experiment_id,run_id,execution_time,model_path\n")
-
-    @patch("utils.logger.os.path.exists")
-    @patch("utils.logger.open", new_callable=mock_open(
-        read_data="timestamp,experiment_name,experiment_id,run_id,execution_time,model_path\nexisting_log_line\n"))
     def test_log_run_details_to_file_appends_to_existing_file(self, mock_file, mock_exists):
-        mock_exists.return_value = True
         log_run_details_to_file("test_experiment", "123", "456", 0.9, "/path/to/model")
+        header_expected = "timestamp,experiment_name,experiment_id,run_id,execution_time,model_path"
+        with open("./mlruns/run_logs.csv", 'r') as file:
+            first_line = file.readline().strip()
+        self.assertEqual(first_line, header_expected)
 
-        handle = mock_file()
-        calls = [unittest.mock.call("timestamp,experiment_name,experiment_id,run_id,execution_time,model_path\n"),
-                 unittest.mock.call().write(unittest.mock.ANY),  # Checks if any second write occurs
-                 unittest.mock.call().truncate()]
-        handle.assert_has_calls(calls, any_order=False)
 
 
 if __name__ == '__main__':
